eda7fa804d1770a0bb66bb19ca308f12fcf9b634
jclie <jtong0003@mymail.lausd.net>
2025-01-26 00:02:22 -0800

    finsihed!!

--- debtrepayment.js
+++ debtrepayment.js
@@ -41,16 +41,46 @@ initialLoanInput.addEventListener('input', function() {
     }
 });
 
-fetch("https://jsonplaceholder.typicode.com/todos", {
-    method: "POST",
-    body: JSON.stringify({
-      userId: 1,
-      title: "Fix my bugs",
-      completed: false
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+document.getElementById('inputForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    console.log("test");
+    const formData = {
+        income: document.getElementById('income').value,
+        loan: document.getElementById('loans').value,
+        interestRate: document.getElementById('interestRates').value,
+        compoundRate: document.getElementById('numInt').value,
+        numYear:document.getElementById('numYear').value,
+    };
+
+    console.log("formData", formData);
+
+    const response = await fetch('https://qr7h5xifmzlvumrz6hcdilbf6a0xtkge.lambda-url.us-west-2.on.aws/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'income': document.getElementById('income').value,
+            'loan': document.getElementById('loans').value,
+            'interestRate': document.getElementById('interestRates').value,
+            'compoundRate': document.getElementById('numInt').value,
+            'numYear':document.getElementById('numYear').value,
+        },
+        body: formData, // Convert the data to JSON
+    })
+    .then((response) => response.json()) // Parse the JSON response
+    .then((json) => {
+        console.log(json);  // Handle response from Lambda
+
+        
+        // Display the result on the page
+        const resultMessage = `
+            <p>Monthly Amount: $${parseFloat(json.amtMonthly).toFixed(2)}</p>
+            <p>Percent Savings: ${parseFloat(json.perSavings).toFixed(2)}%</p>
+        `;
+        document.getElementById("result-message").innerHTML = resultMessage;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        document.getElementById("result-message").textContent = 'An error occurred. Please try again.';
+    });
+});
